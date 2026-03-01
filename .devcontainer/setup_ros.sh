6eecd1969ed0354f3ba75c5df807a2fd4a8db051
Yonatannn <yonatanchiel@gmail.com>
2026-03-01 18:26:25 +0000

    add launch configuration for ball tracker system and update setup script

--- .devcontainer/setup_ros.sh
+++ .devcontainer/setup_ros.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
 echo "cd ros2_ws/ && colcon build" >> ~/.bashrc
+echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
 echo "source install/setup.bash" >> ~/.bashrc
--- ros2_ws/src/ball_tracker/setup.py
+++ ros2_ws/src/ball_tracker/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'ball_tracker'
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
     ],
     install_requires=['setuptools==58.2.0', 'numpy<2.0.0', 'rclpy'],
     zip_safe=True,
