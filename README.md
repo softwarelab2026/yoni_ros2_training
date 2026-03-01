ec24ae2b1c781bd5228219150b6c29c09d1e7f65
Yonatannn <yonatanchiel@gmail.com>
2026-03-01 16:12:11 +0000

    add ball tracking functionality and update documentation
    - Implemented BallTracker class for tracking a ball using camera input.
    - Added subscription to camera images and publishing of velocity commands.
    - Updated README with project overview and how it works.
    - Added requirements.txt for dependencies.
    - Enhanced how_to_run.txt with additional command for checking topic names.
    - Updated devcontainer.json for post-creation command to install requirements.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# turtlesim-training
+
+# Yoantan ROS2 Training
+
+## Overview
+In this project, I built a Python ROS2 node that makes a robot in the Turtlesim simulator automatically follow a ball using image processing and a simple control approach.
+
+## How It Works
+The node subscribes to '/camera/image_raw' to receive camera images.
+Using 'cv_bridge', I convert ROS image messages into OpenCV images.
+
+Finally, the node publishes velocity commands to '/turtle1/cmd_vel', allowing the robot to move and follow the ball.
---
+++ ros2_ws/src/ball_tracker/ball_tracker/tracker.py
@@ -0,0 +1,20 @@
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Twist
+
+class BallTrackers(Node):
+    def __init__(self):
+        super().__init__('ball_tracker_node')
+
+        self._publisher = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+
+        self._subcription = self.create_subscription(
+            Image,
+            'camera/image_raw',
+            self._image_callback,
+            10
+        )
+
+    def _image_callback(self, msg):
+        # ball tracking logic
+        pass
